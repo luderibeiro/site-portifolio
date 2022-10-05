3c5142ad4b427e594b0f730b20f7666fc051d8d7
Lude Ribeiro <ludeyuridrums@gmail.com>
2022-10-05 11:55:22 -0300

    Pimeiro commit portifolio

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container, Content } from './style'
+import { FaLinkedin, FaFacebook, FaInstagram, FaEnvelope, FaGithub } from 'react-icons/fa'
+import ItemContact from '../../../components/ItemContact'
+
+export default function Contact() {
+  return (
+    <Container>
+      <Content>
+        <ItemContact 
+          IconFa={FaLinkedin} 
+          LinkContact="https://www.linkedin.com/in/luderibeiro" 
+        />
+        <ItemContact 
+          IconFa={FaInstagram} 
+          LinkContact="https://www.instagram.com/lude_ribeiro7/" 
+        />
+        <ItemContact 
+          IconFa={FaEnvelope} 
+          LinkContact="ludeyuridrums@gmail.com" 
+        />
+        <ItemContact 
+          IconFa={FaGithub}
+          LinkContact="https://github.com/luderibeiro"
+        />
+      </Content>
+    </Container>
+  )
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,25 @@
+import { FaArrowRight } from 'react-icons/fa'
+import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyle'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+   <Container>
+     <Content>
+      <Infos>
+        <Name>Olá, sou Lude Ribeiro</Name>
+        <Function>FullStack Developer Jr</Function>
+        <Intro>Estudante de Engenharia de Software pela Universidade de Brasília,
+        tenho experiência em desenvolvimento Web com React Js e desenvolvimento Backend em Python com Django e Flask.
+        </Intro>
+        <Link href="projects">
+          <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+        </Link>
+      </Infos>
+      <Logo>
+        <Img src="images/ReadyPlayerMe-Avatar1.jpeg" alt="logo" />
+      </Logo>
+     </Content>
+   </Container>
+  )
+}
